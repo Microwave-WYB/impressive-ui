9542f02b4f380972347a9bc15d887809e315a7eb
Yibo Wei <david_wyb2001@outlook.com>
2025-06-13 15:01:26 -0700

    refactor: add type checking for qt state implementations

--- src/impressive_ui/qt/state.py
+++ src/impressive_ui/qt/state.py
@@ -3,6 +3,10 @@ from typing import Any, Generic, TypeVar, TYPE_CHECKING
 from PySide6.QtCore import QObject, Signal
 
 if TYPE_CHECKING:
+    from impressive_ui.abc.state import (
+        check_state_impl,
+        check_mutable_state_impl,
+    )
     from impressive_ui.abc import AbstractState
 
 T = TypeVar("T")
@@ -23,10 +27,10 @@ class State(QObject, Generic[T]):
     def watch(self, callback: Callable[[T], Any]) -> Callable[[], None]:
         callback(self._value)  # Call immediately with current value
         self.valueChanged.connect(callback)
-        
+
         def disconnect_callback() -> None:
             self.valueChanged.disconnect(callback)
-        
+
         return disconnect_callback
 
     def map(self, mapper: Callable[[T], U], /) -> "AbstractState[U]":
@@ -37,12 +41,16 @@ class State(QObject, Generic[T]):
     def bind(self, target: QObject, property_name: str) -> None:
         # Set initial value
         target.setProperty(property_name, self._value)
-        
+
         # Connect to future changes
         def update_target(v):
             target.setProperty(property_name, v)
-        
+
         self.valueChanged.connect(update_target)
+
+
+if TYPE_CHECKING:
+    check_state_impl(State)
 
 
 class MutableState(State[T], Generic[T]):
@@ -54,3 +62,7 @@ class MutableState(State[T], Generic[T]):
     def update(self, updater: Callable[[T], T]) -> None:
         new_value = updater(self._value)
         self.set(new_value)
+
+
+if TYPE_CHECKING:
+    check_mutable_state_impl(MutableState)
